e2d93af15114b2c1853a6ab38b0db621db0c32f6
AwesomeKoala <Jonaschr.helver@gmail.com>
2022-02-09 12:21:52 +0100

    Initial commit

---
+++ SumOfTwoNum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfTwoNum
+{
+    class Program
+    {
+
+        //You might want to run this program by clicking "ctrl + f5" in visual studio :) Otherwise it might close as soon as it gives you the number.
+
+        public static int Sum(int x, int y)
+        {
+            int z;
+            if (x == y)
+            {
+                z = 3 * (x + y);
+            }
+            else
+            {
+                z = x + y;
+            }
+            return z;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter a second number: ");
+            int numTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n After I apply my magic to these numbers, they become : {0} \n", Sum(numOne, numTwo));
+        }
+    }
+}
